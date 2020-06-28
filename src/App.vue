c781f1a4591438525b3e3133a83eb63ece615fdf
lingzong123 <851253031@qq.com>
2020-06-29 00:16:23 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+     <el-button type="warning">警告按钮</el-button>
+  <el-button type="danger">危险按钮</el-button>
+  </div>
+</template>
+
+<style lang="less">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
